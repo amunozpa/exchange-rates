c3cb1a88b15944e7bf4fab8eb3665f8784854dbe
DAMII <damii@IMACE207-ST30.local>
2020-07-12 09:24:18 -0500

    primer commit

---
+++ Exchange/Exchange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Exchange
+//
+//  Created by DAMII on 7/12/20.
+//  Copyright © 2020 Cibertec. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
