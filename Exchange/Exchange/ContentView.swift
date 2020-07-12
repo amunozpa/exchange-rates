c3510636e132689f13ad0af67bd2fe2f3e9f693c
DAMII <damii@IMACE207-ST30.local>
2020-07-12 15:08:05 -0500

    falta agregar a combo los quotes y guardar rates

--- Exchange/Exchange/ContentView.swift
+++ Exchange/Exchange/ContentView.swift
@@ -8,12 +8,48 @@
 
 import SwiftUI
 
+struct ConsultorResponse: Decodable {
+    let rates: [Coin]
+}
+
+struct Coin: Identifiable, Decodable {
+    let id = UUID()
+    let asset_id_quote: String
+    let rate: Float
+    let time: String
+}
+
+class ConsultorViewModel: ObservableObject {
+    var base = "USD"
+    
+    @Published var monedas = [Coin]()
+
+    func getCoins() {
+        
+   
+        let stringUrl = "https://rest.coinapi.io/v1/exchangerate/\(base)?invert=false&apikey=62ADF072-9054-4214-BCB9-4BBDDFEEEBA3"
+
+        let url = URL(string: stringUrl)!
+
+         let session = URLSession.shared
+        session.dataTask(with: url){
+            (data, response, error) in
+            DispatchQueue.main.async {
+                self.monedas = try! JSONDecoder().decode(ConsultorResponse.self, from: data!).rates
+               // self.base += 20
+            }
+        }.resume()
+    }
+}
+
 struct ContentView: View {
     var colors = ["Red", "Green", "Blue", "Tartan"]
     @State private var selectedColor = 0
     var base = "BTC"
     @State var textQuote = ""
     @ObservedObject var monedaVM = MonedaViewModel()
+    @ObservedObject var coinVM = ConsultorViewModel()
+    
     
     var body: some View {
         NavigationView{
@@ -22,14 +58,6 @@ struct ContentView: View {
                 HStack{
                     Text(self.base)
                     Spacer()
-                    /*Button(action: {
-                        if !self.textQuote.isEmpty {
-                            self.monedaVM.addMoneda(quote: self.textQuote)
-                            self.textQuote = ""
-                        }
-                    }){
-                        Image(systemName: "plus")
-                    }*/
                 }
             }
             Section(header: Text("Moneda Quote")){
